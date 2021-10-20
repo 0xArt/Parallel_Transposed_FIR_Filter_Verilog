67ee211122094cdd336f8022ac4950be86efabca
0xArt <artinisagholian@gmail.com>
2021-10-19 19:17:08 -0700

    added filter plotting in script

--- coeff_maker.py
+++ coeff_maker.py
@@ -7,6 +7,7 @@ Created on Thu Oct 14 14:59:55 2021
 
 from scipy import signal
 import numpy as np
+from pylab import figure, clf, plot, xlabel, ylabel, xlim, ylim, title, grid, axes, show
 
 
 
@@ -23,8 +24,8 @@ def float_to_fixed(num, fractional_bits):
 
 #fir filter coeff creation
 num_taps = 60;
-filter_cutoff = 1e6;
-sampling_freq = 40e6;
+filter_cutoff = 5e6;
+sampling_freq = 50e6;
 nyquist_freq = sampling_freq/2;
 coeff_list = signal.firwin(numtaps=num_taps, cutoff=filter_cutoff, window='blackman', pass_zero=True, fs=sampling_freq)
 
@@ -41,4 +42,17 @@ for i in range(len(fixed_point_coeff)):
 coeff_mem_file.close()
 
 
-
+# Plot Filter
+figure(1)
+plot(coeff_list, "bo-", linewidth=2)
+title("Filter Coefficients (%d Taps)" % num_taps)
+grid(True)
+figure(2)
+clf()
+w, h = signal.freqz(coeff_list, worN=16384)
+plot((w / np.pi / (1e6)) * nyquist_freq, 10 * np.log10(np.absolute(h)), linewidth=2)
+xlabel("Frequency (MHz)")
+ylabel("Gain (dB)")
+title("Frequency Response")
+ylim(-60, 1)
+grid(True)
